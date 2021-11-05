964f9b7fb012af64daeb44706190a2fa187253c0
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-04 22:37:13 -0400

    more stylez

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,32 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+    background-color: orange;
+    border: none;
+    border-radius: 8px;
+    padding: 2%;
+    text-align: center;
+`
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
+
+const StyledHeader = styled.header`
+    padding: 2%;
+`
 
 const Header = (props) => {
     return (
-        <header>
+        <StyledHeader>
             <h1>Welcome to Michael's Nasa Photo of the Day API</h1>
-            <a href='#'>Home</a>
-            <a href='#'>Photo</a>
-            <a href='#'>About</a>
-        </header>
+            <StyledDiv>
+                <StyledButton>Home</StyledButton>
+                <StyledButton>Photo</StyledButton>
+                <StyledButton>About</StyledButton>
+            </StyledDiv>
+        </StyledHeader>
     )
 }
 
